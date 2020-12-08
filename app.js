0a0e2ec88e4c8b509f9fa0c78737c7e1922e77bc
Mitch <mitch@MitchMacBookPro.local>
2020-12-07 17:53:30 -0700

    install ejs, express. Setup views

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
+
+app.listen(3000, function() {
+    console.log("Server listening on port 3000");
+});
