649f1885592da2f0f22c360ddef02edcbc29a740
Mitch <mitch@MitchMacBookPro.local>
2020-12-08 14:30:22 -0700

    Create vulnerability page and style.

--- app.js
+++ app.js
@@ -5,12 +5,24 @@ app.use(express.static(__dirname + "/public"));
 
 app.set('view engine', 'ejs');
 
+app.get('/', function(req, res) {
+    res.render('index.ejs', {criticalCount: 0, highCount: 0, mediumCount: 0, lowCount: 0});
+});
+
 app.get('/login', function(req, res) {
     res.render('login.ejs', {});
 });
 
-app.get('/', function(req, res) {
-    res.render('index.ejs', {criticalCount: 0, highCount: 0, mediumCount: 0, lowCount: 0});
+app.get('/vulnerabilities', function(req, res) {
+    var testData = [{
+        severity: 4,
+        cvss: 10.0,
+        name: "Adobe Reader Unsupported Version",
+        family: "Adobe",
+        pluginId: "012345",
+        count: 10
+    }]
+    res.render('vulnerabilities.ejs', {vulnerabilities: testData});
 });
 
 app.listen(3000, function() {
