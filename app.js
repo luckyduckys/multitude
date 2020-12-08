2d69e451abddaaa077b3e9ab63dc9ede24dc956e
Mitch <mitch@MitchMacBookPro.local>
2020-12-07 19:47:31 -0700

    Create login page.

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 app.set('view engine', 'ejs');
+
+app.get('/login', function(req, res) {
+    res.render("login.ejs", {});
+});
 
 app.listen(3000, function() {
     console.log("Server listening on port 3000");
