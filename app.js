1fede0731ab6ea998202594bbcd93faa8c041e45
Mitch <mitch@MitchMacBookPro.local>
2020-12-08 20:52:11 -0700

    Add manage users, scans, and scanners. Modify static folder to work with subpaths.

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-app.use(express.static(__dirname + "/public"));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 
@@ -21,7 +22,8 @@ app.get('/vulnerabilities', function(req, res) {
         family: "Adobe",
         pluginId: "012345",
         count: 10
-    }]
+    }];
+
     res.render('vulnerabilities.ejs', {vulnerabilities: testData});
 });
 
@@ -32,8 +34,43 @@ app.get('/assets', function(req, res) {
         os: "Windows Server 2019",
         lastScan: "December 4th, 2020",
         scanner: "Home Scanner"
-    }]
+    }];
+
     res.render('assets.ejs', {assets: testData});
+});
+
+app.get('/manage/scanners', function(req, res) {
+    var testData = [{
+        name: "Home Scanner",
+        ip: "192.168.10.110",
+        status: "Online",
+        version: "7.6.12345",
+        type: "Nessus Essentials"
+    }];
+
+    res.render('scanners.ejs', {scanners: testData});
+});
+
+app.get('/manage/users', function(req, res) {
+    var testData = [{
+        firstName: "Billy Bob",
+        lastName: "Thorton",
+        username: "bthorton",
+        email: "bthorton@gmail.com"
+    }];
+
+    res.render('users.ejs', {users: testData});
+});
+
+app.get('/manage/scans', function(req,res) {
+    var testData = [{
+        name: "Home Network Scan",
+        scanner: "Home Scanner",
+        created: "December 4th, 2020",
+        modified: "December 4th, 2020"
+    }];
+
+    res.render('scans.ejs', {scans: testData});
 });
 
 app.listen(3000, function() {
