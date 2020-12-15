5d2f56ca3b070ca6a9258b604413fc33abd0d0bd
luckyduckys <mechammitchell@gmail.com>
2020-12-15 12:17:58 -0700

    Create get scanner properties cronjob

--- cron.js
+++ cron.js
@@ -24,6 +24,35 @@ cron.schedule('*/30 * * * * *', function () {
     });
 });
 
+//Get Server properties every hour.
+cron.schedule('*/45 * * * * *', function() {
+    models.Scanner.find({}, async function(err, scanners) {
+
+        if (err) {
+            console.log(err);
+        }
+
+        for (let i = 0; i < scanners.length; i++) {
+            let scannerProperties = {};
+
+            if (scanners[i].status == 'offline') {
+                console.log('Skipping until online...');
+                continue;
+            }
+
+            scannerProperties = await scannerOps.scannerGetRequest(scanners[i], '/server/properties');
+            
+            if (!scannerProperties.hasOwnProperty('status')) {
+                models.Scanner.updateOne({_id: scanners[i]._id}, {version: scannerProperties.server_version, type: scannerProperties.nessus_type}, function(err, results) {
+                    if (err) {
+                        console.log(err);
+                    }
+                });
+            }
+        }
+    });
+});
+
 module.exports = {
     cron
 }
