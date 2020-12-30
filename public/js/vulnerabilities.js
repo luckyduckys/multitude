0e4430ad5b01777dbe40033e21feea5a175c18d8
luckyduckys <mechammitchell@gmail.com>
2020-12-29 19:25:10 -0700

    Add sort by arrows.

--- public/js/vulnerabilities.js
+++ public/js/vulnerabilities.js
@@ -1,5 +1,29 @@
 $(document).ready(populateTable());
 
+$("th > span.text-nowrap").click(function() {
+    let arrowClasses = $(this).children().attr("class").split(/\s+/);
+    $("th > span.text-nowrap > span").removeClass("fa-sort-down");
+    $("th > span.text-nowrap > span").removeClass("fa-sort-up");
+    $("th > span.text-nowrap > span").addClass("fa-sort");
+
+    if (arrowClasses.includes("fa-sort-down")) {
+        $(this).children().removeClass("fa-sort");
+        $(this).children().removeClass("fa-sort-down");
+        $(this).children().addClass("fa-sort-up");
+    }
+
+    else if (arrowClasses.includes("fa-sort-up")) {
+        $(this).children().removeClass("fa-sort");
+        $(this).children().removeClass("fa-sort-up");
+        $(this).children().addClass("fa-sort-down");
+    }
+
+    else {
+        $(this).children().removeClass("fa-sort");
+        $(this).children().addClass("fa-sort-down");
+    }
+});
+
 function populateTable() {
     $("#vulnsTableBody").empty();
     $("#vulnsTable").addClass("hidden");
